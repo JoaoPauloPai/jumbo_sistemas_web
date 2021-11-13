12235cdfd83b570eb079f20db7ec9a0dc5e0bdeb
Joao paulo Gomes da Silva <jaoopaulopai99@gmail.com>
2021-11-13 08:24:47 -0400

    Primeiro comit do projeto

---
+++ jumbo_sistemas_web/src/main/java/br/com/jumbo/JumboSistemasWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.jumbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumboSistemasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumboSistemasWebApplication.class, args);
+	}
+
+}
---
+++ jumbo_sistemas_web/src/test/java/br/com/jumbo/JumboSistemasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jumbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumboSistemasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
