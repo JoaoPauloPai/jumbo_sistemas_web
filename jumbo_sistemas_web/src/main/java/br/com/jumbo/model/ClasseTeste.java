3268c4a04adfc64ad43fb9bebd89cb658d0401f0
Joao paulo Gomes da Silva <jaoopaulopai99@gmail.com>
2021-12-16 17:03:21 -0400

    Implementacao Classes Modelos 07

---
+++ jumbo_sistemas_web/src/main/java/br/com/jumbo/model/ClasseTeste.java
@@ -0,0 +1,12 @@
+package br.com.jumbo.model;
+
+import java.io.Serializable;
+
+public class ClasseTeste implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
