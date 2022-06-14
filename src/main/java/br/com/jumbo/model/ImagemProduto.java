ca6671f4a805a358d657ba45143f953e7c5f730a
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2022-06-13 20:13:59 -0400

    commit 13-06-22 End Point Produto Atualizando

--- src/main/java/br/com/jumbo/model/ImagemProduto.java
+++ src/main/java/br/com/jumbo/model/ImagemProduto.java
@@ -15,6 +15,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "imagem_produto")
 @SequenceGenerator(name = "seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
@@ -32,25 +34,27 @@ public class ImagemProduto implements Serializable {
 	@Column(columnDefinition = "text", nullable = false)
 	private String imagemMiniatura;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
 	private Produto produto;
 
+	@JsonIgnore
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa;
 
 	/**
 	 * @return the empresa
 	 */
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
 	/**
 	 * @param empresa the empresa to set
 	 */
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
