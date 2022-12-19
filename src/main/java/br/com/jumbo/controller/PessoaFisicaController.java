7312ce7a5a8995ba78922ff0dbb3df0410f0ce65
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2022-12-18 21:30:48 -0400

    Commit Atualizando End point Pessoa Fisica 18-12-22

--- src/main/java/br/com/jumbo/controller/PessoaFisicaController.java
+++ src/main/java/br/com/jumbo/controller/PessoaFisicaController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -108,6 +109,15 @@ public class PessoaFisicaController {
 		List<PessoaFisica> fisicas = pessoaFisicaRepository.pesquisaPorNomePF(nome.trim().toUpperCase());
 		
 		return new ResponseEntity<List<PessoaFisica>>(fisicas, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@DeleteMapping(value = "**/deletePessoaFisicaPorId/{id}")
+	public ResponseEntity<?> deletePessoaFisicaPorId(@PathVariable("id") Long id) {
+
+		pessoaFisicaRepository.deleteById(id);
+
+		return new ResponseEntity("Pessoa Física deletado por Id com sucesso!", HttpStatus.OK);
 	}
 
 }
