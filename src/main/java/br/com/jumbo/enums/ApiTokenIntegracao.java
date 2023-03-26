c7972b65e89b6d83139db2fe2194c55f9c2ebf6e
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2023-03-26 10:12:25 -0400

    commit 26-03-23 atualizando Projeto

---
+++ src/main/java/br/com/jumbo/enums/ApiTokenIntegracao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package br.com.jumbo.enums;
+
+/**
+ * @author João Paulo
+ *
+ * 26 de mar. de 2023
+ * 09:56:35
+ */
+public class ApiTokenIntegracao {
+
+}
--- src/main/java/br/com/jumbo/security/JwtApiAutenticationFilter.java
+++ src/main/java/br/com/jumbo/security/JwtApiAutenticationFilter.java
@@ -9,11 +9,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 /**
